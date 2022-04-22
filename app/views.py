9e34a579f37dd45400bbfa66575422967efd5bb9
Shine May <shinemay.hung@gmail.com>
2022-04-22 03:35:29 +0000

    updated

--- app/views.py
+++ app/views.py
@@ -1,11 +1,14 @@
-from app import app
-from datetime import datetime
+from flask import Flask
+import datetime
 
+app = Flask(__name__)
 @app.route('/owner')
 def owner():
    return "Hello world from Shine May\n"
 @app.route('/datetime')
-def datetime():
-   return  datetime.now().strftime("%d/%m/%Y %H:%M:%S\n")
-
+def timestamp():
+   return  str(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S\n"))
+   #  return "Text"
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=True)
 
